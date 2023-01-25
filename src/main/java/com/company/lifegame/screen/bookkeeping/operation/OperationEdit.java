dc367a70b978cb7ee231824cdb92010f8b8c27f3
Yolshin <elshin195@gmail.com>
2023-01-25 21:29:50 +0700

    Доработка операций и баланса

--- src/main/java/com/company/lifegame/screen/bookkeeping/operation/OperationEdit.java
+++ src/main/java/com/company/lifegame/screen/bookkeeping/operation/OperationEdit.java
@@ -1,11 +1,19 @@
 package com.company.lifegame.screen.bookkeeping.operation;
 
-import com.company.lifegame.service.DateService;
-import io.jmix.ui.component.DateField;
-import io.jmix.ui.screen.*;
+import com.company.lifegame.entity.bookkeeping.Account;
 import com.company.lifegame.entity.bookkeeping.Operation;
+import com.company.lifegame.entity.bookkeeping.OperationType;
+import com.company.lifegame.service.DateService;
+import com.company.lifegame.service.bookkeeping.BalanceService;
+import io.jmix.core.EntityStates;
+import io.jmix.ui.component.DateField;
+import io.jmix.ui.component.EntityPicker;
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.component.TextField;
+import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @UiController("lg_Operation.edit")
@@ -14,13 +22,75 @@ import java.time.LocalDateTime;
 public class OperationEdit extends StandardEditor<Operation> {
 
     @Autowired
+    private EntityStates entityStates;
+
+    @Autowired
     private DateService dateService;
     @Autowired
+    private BalanceService balanceService;
+
+    @Autowired
     private DateField<LocalDateTime> dateField;
+
+    @Autowired
+    private TextField<BigDecimal> valueOneField;
+
+    @Autowired
+    private EntityPicker<Account> accountOneField;
+
+    @Autowired
+    private TextField<BigDecimal> valueTwoField;
+
+    @Autowired
+    private EntityPicker<Account> accountTwoField;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
         dateField.setValue(dateService.now());
+    }
+
+    @Subscribe
+    public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
+
+        // Применяем операцию к счетам
+        if (entityStates.isNew(getEditedEntity())) {
+            balanceService.perform(getEditedEntity());
+        }
+    }
+
+    @Subscribe("typeField")
+    public void onTypeFieldValueChange(HasValue.ValueChangeEvent<OperationType> event) {
+        OperationType type = event.getValue();
+        if (type == null) {
+            setVisibleIncome(true);
+            setVisibleExpense(true);
+            return;
+        }
+
+        switch (type) {
+            case INCOME -> {
+                setVisibleIncome(true);
+                setVisibleExpense(false);
+            }
+            case EXPENSE -> {
+                setVisibleIncome(false);
+                setVisibleExpense(true);
+            }
+            case TRANSFER -> {
+                setVisibleIncome(true);
+                setVisibleExpense(true);
+            }
+        }
+    }
+
+    void setVisibleIncome(boolean visible) {
+        accountTwoField.setVisible(visible);
+        valueTwoField.setVisible(visible);
+    }
+
+    void setVisibleExpense(boolean visible) {
+        accountOneField.setVisible(visible);
+        valueOneField.setVisible(visible);
     }
 
 }
