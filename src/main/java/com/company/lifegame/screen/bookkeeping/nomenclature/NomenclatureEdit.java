b22878612e1066d52d2e51c7e6f4e032d2cd4879
Yolshin <elshin195@gmail.com>
2023-01-28 01:58:48 +0700

    Обновление до версии 1.4.3
    Автозаполнение при создании новой номенклатуры на основе базовой.

--- src/main/java/com/company/lifegame/screen/bookkeeping/nomenclature/NomenclatureEdit.java
+++ src/main/java/com/company/lifegame/screen/bookkeeping/nomenclature/NomenclatureEdit.java
@@ -1,10 +1,41 @@
 package com.company.lifegame.screen.bookkeeping.nomenclature;
 
-import io.jmix.ui.screen.*;
+import com.company.lifegame.entity.bookkeeping.Category;
 import com.company.lifegame.entity.bookkeeping.Nomenclature;
+import com.company.lifegame.entity.bookkeeping.UnitEnum;
+import io.jmix.ui.component.ComboBox;
+import io.jmix.ui.component.EntityPicker;
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
 
 @UiController("lg_Nomenclature.edit")
 @UiDescriptor("nomenclature-edit.xml")
 @EditedEntityContainer("nomenclatureDc")
 public class NomenclatureEdit extends StandardEditor<Nomenclature> {
+    @Autowired
+    private EntityPicker<Category> categoryField;
+
+    @Autowired
+    private ComboBox<UnitEnum> unitField;
+
+    @Subscribe("parentField")
+    public void onParentFieldValueChange(HasValue.ValueChangeEvent<Nomenclature> event) {
+        boolean valid = event.getValue() != null;
+
+        if (valid) {
+            Category category = event.getValue().getCategory();
+            if (category != null && !Objects.equals(categoryField.getValue(), category)) {
+                categoryField.setValue(event.getValue().getCategory());
+            }
+
+            UnitEnum unit = event.getValue().getUnit();
+            if (unit != null && !Objects.equals(unitField.getValue(), unit)) {
+                unitField.setValue(unit);
+            }
+        }
+    }
+
 }
